7e7c293b763714b0f2b21039ad3ecd2aa0f6707c
Maria Khomenko <maria@Marias-MBP.home>
2025-12-01 20:38:56 -0800

    first day puzzles

--- 1/part2.py
+++ 1/part2.py
@@ -7,20 +7,28 @@ for i, l in enumerate(f.readlines()):
   n = int(l[1:])
   entries.append((direction, n))
 
-
 current = 50
 count = 0
 
 for e in entries:
     if e[0] == 'R':
-        current = (current + e[1]) % 100
+        current = (current + e[1])
+        if current > 99:
+            rots = int(current / 100)
+            current = current % 100
+            count += rots 
     if e[0] == 'L':
+        rot = int(e[1] / 100)
+        prev_current = current
         current = (current - (e[1] %100))
+        count += rot
+        if current == 0:
+            count += 1
         if current < 0:
             current = 100 + current
+            if prev_current != 0:
+                count += 1
 
-    if current == 0:
-        count+= 1
 
 print(count)
 
